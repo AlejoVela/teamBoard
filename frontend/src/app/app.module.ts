013b1a11572651ea18eca16e29d3d41bbe99d426
Diego R <alejorvela@gmail.com>
2021-08-24 15:16:19 -0500

    configurations finished (services, routing, components and guards added,)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,23 @@ import { HeaderComponent } from './home/header/header.component';
 import { LoginComponent } from './home/login/login.component';
 import { RegisterComponent } from './home/register/register.component';
 import { FooterComponent } from './home/footer/footer.component';
+import { ListTaskComponent } from './board/list-task/list-task.component';
+import { SaveTaskComponent } from './board/save-task/save-task.component';
+import { ListUserComponent } from './admin/list-user/list-user.component';
+import { RegisterUserComponent } from './admin/register-user/register-user.component';
+import { UpdateUserComponent } from './admin/update-user/update-user.component';
+import { RegisterRoleComponent } from './admin/register-role/register-role.component';
+import { ListRoleComponent } from './admin/list-role/list-role.component';
+import { UpdateRoleComponent } from './admin/update-role/update-role.component';
+
+//services
+import { UserService } from './services/user.service';
+import { RoleService } from './services/role.service';
+import { BoardService } from './services/board.service';
+import { TokenInterceptorService } from './services/token-interceptor.service';
+
+//guards
+import { AuthGuard } from "./guard/auth.guard";
 
 @NgModule({
   declarations: [
@@ -14,13 +31,18 @@ import { FooterComponent } from './home/footer/footer.component';
     HeaderComponent,
     LoginComponent,
     RegisterComponent,
-    FooterComponent
+    FooterComponent,
+    ListTaskComponent,
+    SaveTaskComponent,
+    ListUserComponent,
+    RegisterUserComponent,
+    UpdateUserComponent,
+    RegisterRoleComponent,
+    ListRoleComponent,
+    UpdateRoleComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [UserService, RoleService, BoardService, TokenInterceptorService, AuthGuard],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
